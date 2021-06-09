58e238a5f8b2e7eddbddd2898c3452aee55eda54
myagiz <myagiz24@gmail.com>
2021-06-09 16:57:42 +0300

    date

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import { Segment } from 'semantic-ui-react'
+import React from "react";
+import { Segment } from "semantic-ui-react";
 
 export default function Footer() {
-    return (
-        <div>
-             <Segment >Contact: <a href="https://github.com/myagiz" target="blank">
-              Created by Muhammet Yağız - GitHub Adress
-            </a>
-            <br/>
-            <br/>
-             <b>Copyright 2021. Human Resource Management System.Builded with Spring Boot and React</b> </Segment>
-        </div>
-        
-    )
+  return (
+    <Segment>
+      Contact:{" "}
+      <a href="https://github.com/myagiz" target="blank">
+        Created by Muhammet Yağız - GitHub Adress
+      </a>
+      <br />
+      <br />
+      <b>
+        Copyright 2021. Human Resource Management System.Builded with Spring
+        Boot and React
+      </b>{" "}
+    </Segment>
+  );
 }
--- src/layouts/jobSeeker/JobSeekerCreateCVForm.jsx
+++ src/layouts/jobSeeker/JobSeekerCreateCVForm.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import {
-  Segment,
-  Form,
-  TextArea,
-  Input,
-  Button,
-  Checkbox,
-} from "semantic-ui-react";
+import { Segment, Form, Button } from "semantic-ui-react";
+import CVGeneral from "./CV/CVGeneral";
+import CVSchool from "./CV/CVSchool";
+import CVWorkplace from "./CV/CVWorkplace";
+import CVLanguage from "./CV/CVLanguage";
 
 export default function JobSeekerCreateCVForm() {
   return (
@@ -17,124 +14,10 @@ export default function JobSeekerCreateCVForm() {
       <br />
       <div align="left">
         <Form>
-          <Segment color="violet">
-            <h5>Genel Bilgileriniz</h5>
-            <Form.Field
-              id="form-textarea-control-opinion"
-              control={TextArea}
-              placeholder="Ön Yazı Metin Bölgeniz..."
-            />
-            <Form.Field
-              id="form-input-control-first-name"
-              control={Input}
-              placeholder="Bilgisayar Yetenekleriniz"
-            />
-            <Form.Group widths="equal">
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="Linkedin Profiliniz"
-              />
-              <Form.Field
-                id="form-input-control-last-name"
-                control={Input}
-                placeholder="GitHub Profiliniz"
-              />
-            </Form.Group>
-            <Button color="violet" size="tiny">Resim Yükle</Button>
-          </Segment>
-
-          <Segment color="red">
-            <h5>Okul Bilgileriniz</h5>
-            <Form.Group widths="equal">
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="Okul Adı"
-              />
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="Bölüm Adı"
-              />
-            </Form.Group>
-            <Form.Group widths="equal">
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="Başlangıç Tarihi"
-              />
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="Bitiş Tarihi"
-              />
-            </Form.Group>
-            <Form.Field control={Checkbox} label="Okulum hala devam ediyor" />
-            <div align="right">
-              <Button color="red" size="small">
-                Okul Detaylarını Ekle
-              </Button>
-            </div>
-          </Segment>
-
-          <Segment color="blue">
-            <h5>İş Deneyimleriniz</h5>
-            <Form.Group widths="equal">
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="İşyeri Adı"
-              />
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="İşyeri Pozisyonu"
-              />
-            </Form.Group>
-            <Form.Group widths="equal">
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="Başlangıç Tarihi"
-              />
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="Bitiş Tarihi"
-              />
-            </Form.Group>
-            <Form.Field
-              control={Checkbox}
-              label="Çalışmaya hala devam ediyorum"
-            />
-            <div align="right">
-              <Button color="blue" size="small">
-                İş Deneyimi Detaylarını Ekle
-              </Button>
-            </div>
-          </Segment>
-          <Segment color="orange">
-            <h5>Yabancı Dil Bilgileriniz</h5>
-            <Form.Group widths="equal">
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="Yabancı Dil Adı"
-              />
-              <Form.Field
-                id="form-input-control-first-name"
-                control={Input}
-                placeholder="Beceri Puanı"
-              />
-            </Form.Group>
-
-            <div align="right">
-              <Button color="orange" size="small">
-                Yabancı Dil Bilgilerini Ekle
-              </Button>
-            </div>
-          </Segment>
+          <CVGeneral></CVGeneral>
+          <CVSchool></CVSchool>
+          <CVWorkplace></CVWorkplace>
+          <CVLanguage></CVLanguage>
           <br />
           <Button positive attached="bottom" size="large">
             Özgeçmiş Bilgilerimi Kaydet
